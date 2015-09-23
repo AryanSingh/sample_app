e2f697b35cb48688101376046e02b08940d46b70
AryanSingh <aryan@iitgn.ac.in>
2015-09-23 16:57:24 +0530

    add a static page contoller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -12,7 +16,7 @@ Rails.application.routes.draw do
 
   root 'application#hello'
 
->>>>>>> origin/master
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
